343d6ec440b508ae58c7a056116ae789845a59ca
William01br <williamstcorreia123@gmail.com>
2025-07-22 13:26:02 -0300

    chore: add eslint and prettier config

---
+++ backend/eslint.config.mjs
@@ -0,0 +1,33 @@
+import globals from "globals";
+import eslint from "@eslint/js";
+import tsEslint from "typescript-eslint";
+import eslintConfigPrettier from "eslint-config-prettier";
+
+export default tsEslint.config(
+  {
+    ignores: ["node_modules/**"],
+  },
+  eslint.configs.recommended,
+  tsEslint.configs.recommended,
+  {
+    files: ["**/*.ts"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 5,
+      sourceType: "module",
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  eslintConfigPrettier
+);
